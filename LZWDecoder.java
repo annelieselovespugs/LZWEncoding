466281f88fda6a966515f620796a5fc8999bfe10
jasminew0528-work <53498627+jasminew0528-work@users.noreply.github.com>
2020-09-29 20:56:28 -0700

    changed dictionary from arraylist to hashmap

--- LZWDecoder.java
+++ LZWDecoder.java
@@ -23,23 +23,23 @@ public class LZWDecoder {
 		FileWriter file = new FileWriter ("decodedText.txt");
 		
 		
-		// create an arraylist that will be our dictionary (index of a string = its code)
-		ArrayList<String> decodingDictionary = new ArrayList<String>();
+		// create an hashmap that will be our dictionary (index of a string = its code)
+		HashMap<String, String> decodingDictionary = new HashMap<String, String>();
 		// initialize it by setting characters 0-255 with the ascii table
 		for (int i = 0; i < 256; i ++)
 		{
 			String letter = "" + (char)i;
-			decodingDictionary.add(letter);
+			decodingDictionary.put(""+i, letter);
 		}
 		
 		//represents the past code from the input file
-		int past;
+		String past;
 		
 		//represents the current code from the input file
-		int current;
+		String current;
 		
 		//read in the next number
-			//this string will hold one line from the input file (ie, a number)
+			//this string will hold one line from the input file (ie, a number representing the code)
 		String num = "";
 			//read in the next character
 		int currentChar = br.read();
@@ -50,7 +50,7 @@ public class LZWDecoder {
 			currentChar = br.read();
 		}
 		//turn the string into the number (ie the code) and set it as the "past" code
-		past = Integer.parseInt(num);
+		past = "" + num;
 		
 		//output the corresponding char to that number 
 		file.write(decodingDictionary.get(past));
@@ -68,24 +68,23 @@ public class LZWDecoder {
 			}
 			
 			//set that number as the "current" code
-			current = Integer.parseInt(num);
+			current = "" + num;
 			
 			//check if the "current" code is in the dictionary
-			if (decodingDictionary.size() > current)
+			if (decodingDictionary.containsKey (current))
 			{
 				//if it is, output the corresponding phrase to the output file
 				file.write(decodingDictionary.get(current));
-				//you know that the original encoding must have added the phrase corresponding to the past code
-				//+ the first letter of the current code to the table (cuz it reads letter by letter)
-				//so add the phrase corresponding to the past code + the first letter of the current code to 
-				//the dictionary
+				//add the phrase corresponding to the past code + the first letter of the current code to the dictionary
 				String stringToAdd = decodingDictionary.get(past) + decodingDictionary.get(current).substring(0,1);
-				decodingDictionary.add(stringToAdd);
+				decodingDictionary.put(""+decodingDictionary.size(), stringToAdd);
 			}
 			
 			else
 			{
 				//if the "current" code isn't in the dictionary
+				//since encoder is always one step ahead,
+				//current code is not in the dictionary yet 
 				//this only occurs if there's a pattern where the first letter of the last "phrase" directly
 				//follows it (cuz the decompression is one "step" behind)
 				//therefore, we can determine the phrase corresponding to the unknown code by
@@ -94,7 +93,7 @@ public class LZWDecoder {
 				file.write(outputString);
 				
 				//then add the corresponding phrase to the current code to the dictionary
-				decodingDictionary.add(outputString);
+				decodingDictionary.put(""+decodingDictionary.size(), outputString);
 			}
 			
 			//make the current code the past code
@@ -104,7 +103,7 @@ public class LZWDecoder {
 		//close the buffered reader
 		br.close();
 		
-		//
+		
 		file.close();
 	}
 }
