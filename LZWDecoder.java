6859d3c030cc95383c477cbfa57c5875ad397565
annelieselovespugs <annelieseardizzone@Brynhildr.local>
2020-10-05 23:56:53 -0700

    Final Changes
    final changes babey B)
    
    Everything should fit with the court-ordered standards as found on the hub.
    
    Also added a basic tester that explains how to use the program encode/decode stuff.

--- LZWDecoder.java
+++ LZWDecoder.java
@@ -1,3 +1,9 @@
+/*
+* Praise be to Ms. Kaufman and Computer Science A teachers.
+* They spoke the truth when they spoke of handwritten code and BlueJ.
+*/
+
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -5,10 +11,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-public class LZWDecoder {
+public class LZWDecoder{
 	
-	public static void main (String[]args) throws IOException
-	{
+	public static void main (String[]args) throws IOException{
 		//determine what text file the user wants to decode by asking them
 		String fileName;
 		Scanner keyboard = new Scanner (System.in);
@@ -17,7 +22,7 @@ public class LZWDecoder {
 		
 
 		//read in the file to decode
-		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		BufferedReader bR = new BufferedReader(new FileReader(fileName));
 		
 		//create the file writer to produce the decoded file
 		FileWriter file = new FileWriter ("decodedText.txt");
@@ -26,8 +31,7 @@ public class LZWDecoder {
 		// create an hashmap that will be our dictionary (index of a string = its code)
 		HashMap<String, String> decodingDictionary = new HashMap<String, String>();
 		// initialize it by setting characters 0-255 with the ascii table
-		for (int i = 0; i < 256; i ++)
-		{
+		for (int i = 0; i < 256; i ++){
 			String letter = "" + (char)i;
 			decodingDictionary.put(""+i, letter);
 		}
@@ -38,16 +42,16 @@ public class LZWDecoder {
 		//represents the current code from the input file
 		String current;
 		
-		//read in the next number
-			//this string will hold one line from the input file (ie, a number representing the code)
+		/*read in the next number
+		 * this string will hold one line from the input file 
+		 * (ie, a number representing the code)*/
 		String num = "";
 			//read in the next character
-		int currentChar = br.read();
+		int currentChar = bR.read();
 			//continue reading until you hit a line break (indicating the number is over)
-		while (currentChar!= 10)
-		{
+		while (currentChar!= 10){
 			num += (char)(currentChar);
-			currentChar = br.read();
+			currentChar = bR.read();
 		}
 		//turn the string into the number (ie the code) and set it as the "past" code
 		past = "" + num;
@@ -56,40 +60,40 @@ public class LZWDecoder {
 		file.write(decodingDictionary.get(past));
 		
 				
-		while (br.ready() && decodingDictionary.size() < 10000)
-		{
+		while (bR.ready() && decodingDictionary.size() < 10000){
 			//read in the next number using the same method i described above (just copy pasted)
 			num = "";
-			currentChar = br.read();
-			while (currentChar!=10)
-			{
+			currentChar = bR.read();
+			while (currentChar!=10){
 				num += (char)(currentChar);
-				currentChar = br.read();
+				currentChar = bR.read();
 			}
 			
 			//set that number as the "current" code
 			current = "" + num;
 			
 			//check if the "current" code is in the dictionary
-			if (decodingDictionary.containsKey (current))
-			{
+			if (decodingDictionary.containsKey (current)){
 				//if it is, output the corresponding phrase to the output file
 				file.write(decodingDictionary.get(current));
-				//add the phrase corresponding to the past code + the first letter of the current code to the dictionary
-				String stringToAdd = decodingDictionary.get(past) + decodingDictionary.get(current).substring(0,1);
+				/*add the phrase corresponding to the past code + 
+				 * the first letter of the current code to the dictionary*/
+				String stringToAdd = decodingDictionary.get(past); 
+				stringToAdd += decodingDictionary.get(current).substring(0,1);
 				decodingDictionary.put(""+decodingDictionary.size(), stringToAdd);
 			}
 			
-			else
-			{
-				//if the "current" code isn't in the dictionary
-				//since encoder is always one step ahead,
-				//current code is not in the dictionary yet 
-				//this only occurs if there's a pattern where the first letter of the last "phrase" directly
-				//follows it (cuz the decompression is one "step" behind)
-				//therefore, we can determine the phrase corresponding to the unknown code by
-				//adding the first letter of the previous code to the end of the previous code
-				String outputString = decodingDictionary.get(past) + decodingDictionary.get(past).substring(0,1);
+			else{
+				/*if the "current" code isn't in the dictionary
+				 * since encoder is always one step ahead,
+				 * current code is not in the dictionary yet 
+				 * this only occurs if there's a pattern where the first letter 
+				 * of the last "phrase" directly follows it (cuz the decompression 
+				 * is one "step" behind)
+				 * therefore, we can determine the phrase corresponding to the unknown code by
+				 * adding the first letter of the previous code to the end of the previous code*/
+				String outputString = decodingDictionary.get(past);
+				outputString += decodingDictionary.get(past).substring(0,1);
 				file.write(outputString);
 				
 				//then add the corresponding phrase to the current code to the dictionary
@@ -101,9 +105,7 @@ public class LZWDecoder {
 		}
 		
 		//close the buffered reader
-		br.close();
-		
-		
+		bR.close();
 		file.close();
 	}
 }
--- LZWEncoding.java
+++ LZWEncoding.java
@@ -1,6 +1,10 @@
+/*
+* Praise be to Ms. Kaufman and Computer Science A teachers.
+* They spoke the truth when they spoke of handwritten code and BlueJ.
+*/
+
 //Maya Doyle and Anneliese Ardizzone
 //Optimized by Jasmine Wang and Sally Ho
-
 
 
 import java.io.BufferedReader;
@@ -13,50 +17,44 @@ public class LZWEncoding{
 	public static void main (String[] args){
 		//read in a txt file and dump it into a string
 		Scanner keyboard = new Scanner(System.in);
-		System.out.print ("Please give me the name of the file you wish to encode: ");
+		System.out.print("Please give me the name of the file you wish to encode: ");
 		String theFileName = keyboard.nextLine();
-
-
+		int prevPlusCurLen = 0;
 
 		try {
 			//reading in a text file and creating print writer
-			FileReader fr = new FileReader (theFileName);
-			BufferedReader br = new BufferedReader(fr);
-			PrintWriter pw = new PrintWriter ("encoded.txt");
-			String previousCharacter = ""+(char)(br.read());
+			FileReader fR = new FileReader(theFileName);
+			BufferedReader bR = new BufferedReader(fR);
+			PrintWriter pW = new PrintWriter("encoded.txt");
+			String previousCharacter = ""+(char)(bR.read());
 			
 			//create a hashmap as the dictionary that will hold all additional entries after 255
 			HashMap <String, String> theDictionary = new HashMap<String, String>();
 
 			//counter will keep track of what the code for each new entry should be
 			int counter = 256;
-			while (br.ready())	
-			{
-				String currentCharacter = ""+ (char)br.read();
+			while (bR.ready()){
+				String currentCharacter = ""+ (char)bR.read();
 				String previousPlusCurrent = previousCharacter+currentCharacter;
 				
-				//if previousCurrent is already in the theDictionary or if it's in the ascii table, update previous
-				if (theDictionary.containsKey(previousPlusCurrent) || previousPlusCurrent.length() == 1)
-				{
+				/*if previousCurrent is already in the theDictionary or 
+				 * if it's in the ascii table, update previous*/
+				prevPlusCurLen = previousPlusCurrent.length();
+				if (theDictionary.containsKey(previousPlusCurrent) || prevPlusCurLen == 1){
 					previousCharacter = previousPlusCurrent;
 				}
 				//print out value for previous character
-				else
-				{
+				else{
 					//print previous character if it's a single character
-					if (previousCharacter.length()==1)
-					{
-						pw.print((int)previousCharacter.charAt(0) + "\n");
+					if (previousCharacter.length()==1){
+						pW.print((int)previousCharacter.charAt(0) + "\n");
 					}
 					//print previous if it's not a single character
-					else 
-					{
-						pw.print(theDictionary.get(previousCharacter) + "\n");
+					else{
+						pW.print(theDictionary.get(previousCharacter) + "\n");
 					}
 					//add previous + current to dictionary 
-					if (theDictionary.size()<=1000)
-					{
-					
+					if (theDictionary.size()<=1000){
 						theDictionary.put(previousPlusCurrent, ""+counter);
 						counter++;
 					}
@@ -65,137 +63,19 @@ public class LZWEncoding{
 			}
 			
 			//edge case for last entry 
-			if (previousCharacter.length() == 1 )
-			{
-				pw.print((int)previousCharacter.charAt(0)+ "\n");
+			if (previousCharacter.length() == 1 ){
+				pW.print((int)previousCharacter.charAt(0)+ "\n");
 			}
-			else
-			{
-				pw.print(theDictionary.get(previousCharacter) + "\n");
+			else{
+				pW.print(theDictionary.get(previousCharacter) + "\n");
 			}
-			pw.close();
-			br.close();
-			fr.close();
+			pW.close();
+			bR.close();
+			fR.close();
 		}
 
-			catch (IOException e)
-			{
-				System.out.println ("can't read");
-			}
+		catch (IOException error){
+			System.out.println ("can't read");
+		}
 	}
 }
-		
-		
-
-/*
-import java.util.*;
-import java.io.*;
-
-public class LZWEncoding{
-	public static void main (String[] args){
-		//read in a txt file and dump it into a string
-		Scanner keyboard = new Scanner(System.in);
-    	System.out.print ("Please give me the name of the file you wish to encode: ");
-    	String theFileName = keyboard.nextLine();
-    	
-		String fileToEncode = ""; 
-		try{
-			FileReader fileRead = new FileReader (theFileName);
-			char currentLetterInFileToEncode = (char)(fileRead.read());
-			while ((int)currentLetterInFileToEncode < 256){
-				fileToEncode += currentLetterInFileToEncode;
-				currentLetterInFileToEncode = (char)(fileRead.read());
-			}
-			fileRead.close();
-		}
-		catch(IOException ex){
-			System.out.println ("Something went wrong. :("); 
-		}
-
-
-		//--------------------------------------------------------------------------------------
-
-
-		//set up a ArrayList<String> theDictionary = new ArrayList<String>();
-			//characters 0-255 are pre-existing ASCII characters, so the indexes have to start at 256
-				//we don't ACTUALLY have to start at 256, we can start at 0 and just add 256 whenever using that number
-			//it could also be an arraylist of lists (String code, int index), but we can figure out the index based on the code's place in the list (256+indexOf(code)) so there's no need
-		ArrayList<String> theDictionary = new ArrayList<String>();
-
-		//set up an ArrayList<Integer> encodedValues = new ArrayList<Integer>();
-			//this is where the numbers get yeeted in the loop if they're not found
-			//using the completed version of this array, you can essentially remake the whole P, C, P+C table
-		ArrayList<Integer> encodedValues = new ArrayList<Integer>();
-
-		//set up 3 word variables: previous character, current character, and previous + current character
-			
-		String previousCharacter = "";
-		String currentCharacter = "";
-		String previousPlusCurrent = ""; 
-		for(int k=0; k<fileToEncode.length(); k++) {
-			currentCharacter = String.valueOf(fileToEncode.charAt(k));
-			previousPlusCurrent = previousCharacter + currentCharacter;
-			
-			//if found
-			if(previousPlusCurrent.length () == 1 || theDictionary.contains(previousPlusCurrent)) {
-				previousCharacter = previousCharacter + currentCharacter;
-			}
-
-			//if not found
-			else {
-				
-				//adds P+C to dictionary 
-				theDictionary.add(previousPlusCurrent);
-			
-				//output code for previousCharacter to the code stream 
-				if(previousCharacter.length()==1) {
-					encodedValues.add((int)previousCharacter.charAt(0));
-				}
-
-				else if (previousCharacter.length() > 1){ //there was a problem in which the previous character would be added to the dictionary if it was "". "" has length 0, so the "if length() > 1" eliminates the problem
-					encodedValues.add(theDictionary.indexOf(previousCharacter)+256);
-				}
-				previousCharacter = currentCharacter;
-			}
-		}
-
-		//adding on the last current character so we don't lose it
-		if(currentCharacter.length()==1) {
-			encodedValues.add((int)currentCharacter.charAt(0));
-		}
-
-		else if (previousCharacter.length() > 1){
-			encodedValues.add(theDictionary.indexOf(currentCharacter)+256);
-		}
-
-		try {
-			FileWriter writeToFile = new FileWriter("output1.txt");
-			
-			for (int i = 0; i < encodedValues.size(); i++){
-				writeToFile.write(encodedValues.get(i) + "\n");
-			}
-			writeToFile.close();
-		}
-		catch (IOException e) {
-			System.out.println("Uh oh :(");
-		}
-		
-		//--------------------------------------------------------------------------------------
-
-
-		//Look for (previous + current) in the dictionary
-			//a sorting algorithm could be nice but skip it if speed doesn't matter
-				//isn't .sort(list) a thing?
-			//IF FOUND
-				//P = concatination(P,C) (which means previous = previous + current ?) concatenation
-
-			//IF NOT FOUND:
-				//"output the code which denotes P to the codestream" (which means yeet it into the encodedValues array ?)
-				//add the string concatination(previous + current) to the dictionary
-				//set previous = current
-	
-		
-
-	}
-}
-*/
